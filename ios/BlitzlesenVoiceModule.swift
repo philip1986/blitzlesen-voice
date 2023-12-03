a0703e9765f06d6533d0b1c9c2e11cf2d7b2abc7
Philip <>
2023-12-03 18:59:38 +0100

    also check mic permissions

--- ios/BlitzlesenVoiceModule.swift
+++ ios/BlitzlesenVoiceModule.swift
@@ -17,10 +17,10 @@ struct ListenForError: Record {
 }
 
 extension Optional {
-    func `let`(_ do: (Wrapped)->()) {
-        guard let v = self else { return }
-        `do`(v)
-    }
+  func `let`(_ do: (Wrapped) -> Void) {
+    guard let v = self else { return }
+    `do`(v)
+  }
 }
 
 public class BlitzlesenVoiceModule: Module {
@@ -37,45 +37,48 @@ public class BlitzlesenVoiceModule: Module {
     AsyncFunction("listenFor") {
       (
         locale: String, target: [[String]], timeout: Int,
-        onDeviceRecognition: Bool, mistakeConfig: [String: Int], promise: Promise
+        onDeviceRecognition: Bool, mistakeConfig: [String: Int], firstItemDurationOffset: Int,
+        promise: Promise
       ) in
-        
-        voice?.stopRecording()
 
-        if(voice == nil || voice?.locale != locale ) {
+      voice?.stopRecording()
+
+      if voice == nil || voice?.locale != locale {
+        voice = Voice(locale: locale, sendEvent: sendEvent)
+      }
+
+      try voice?.startRecording(
+        target: target, timeout: timeout,
+        onDeviceRecognition: onDeviceRecognition, mistakeConfig: mistakeConfig,
+        firstItemDurationOffset: firstItemDurationOffset
+      ) { error, isCorrect, recognisedText, words in
+        promise.resolve([
+          ListenForError(error: Field(wrappedValue: error?.localizedDescription)),
+          ListenForResponse(
+            isCorrect: Field(wrappedValue: isCorrect),
+            recognisedText: Field(wrappedValue: recognisedText),
+            words: Field(wrappedValue: words)),
+        ])
+      }
+
+    }
+
+    AsyncFunction("train") {
+      (locale: String, phrases: [String], graphemeToPhonems: [[String]], promise: Promise) in
+      if #available(iOS 17, *) {
+        if voice == nil {
           voice = Voice(locale: locale, sendEvent: sendEvent)
         }
-        
-        try voice?.startRecording(
-            target: target, timeout: timeout,
-            onDeviceRecognition: onDeviceRecognition, mistakeConfig: mistakeConfig
-        ) { error, isCorrect, recognisedText, words in
-            promise.resolve([
-                ListenForError(error: Field(wrappedValue: error?.localizedDescription)),
-                ListenForResponse(
-                    isCorrect: Field(wrappedValue: isCorrect),
-                    recognisedText: Field(wrappedValue: recognisedText),
-                    words: Field(wrappedValue: words)),
-            ])
+        Task.detached {
+          await self.voice?.train(
+            locale: locale, phrases: phrases, graphemeToPhonems: graphemeToPhonems)
+          promise.resolve()
         }
-        
-    }
-      
-      AsyncFunction("train") { (locale: String, phrases: [String], graphemeToPhonems: [[String]], promise: Promise) in
-          if #available(iOS 17, *) {
-              if(voice == nil) {
-                voice = Voice(locale: locale, sendEvent: sendEvent)
-              }
-              Task.detached {
-                  await self.voice?.train(locale: locale, phrases: phrases, graphemeToPhonems: graphemeToPhonems)
-                promise.resolve()
-              }
-          } else {
-              promise.resolve()
-          }
-          
-          
+      } else {
+        promise.resolve()
       }
+
+    }
 
     Function("isListening") { () in
       voice?.isListening()
@@ -113,13 +116,14 @@ public class Voice {
   private let audioEngine = AVAudioEngine()
   private let audioSession = AVAudioSession.sharedInstance()
 
-    @available(iOS 17, *)
-    private var lmConfiguration: SFSpeechLanguageModel.Configuration {
-        let outputDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-        let dynamicLanguageModel = outputDir.appendingPathComponent("LM")
-        let dynamicVocabulary = outputDir.appendingPathComponent("Vocab")
-        return SFSpeechLanguageModel.Configuration(languageModel: dynamicLanguageModel, vocabulary: dynamicVocabulary)
-    }
+  @available(iOS 17, *)
+  private var lmConfiguration: SFSpeechLanguageModel.Configuration {
+    let outputDir = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    let dynamicLanguageModel = outputDir.appendingPathComponent("LM")
+    let dynamicVocabulary = outputDir.appendingPathComponent("Vocab")
+    return SFSpeechLanguageModel.Configuration(
+      languageModel: dynamicLanguageModel, vocabulary: dynamicVocabulary)
+  }
 
   init(locale: String, sendEvent: @escaping (String, [String: Any]) -> Void) {
     if Voice.hasPermissions == false { Voice.getPermissions { _ in } }
@@ -134,7 +138,22 @@ public class Voice {
     return recognitionTask != nil
   }
 
-  static func getPermissions(completion: @escaping (Bool) -> Void) {
+  static func getMicPermissions(completion: @escaping (Bool) -> Void) {
+    switch AVAudioSession.sharedInstance().recordPermission {
+    case .granted:
+      completion(true)
+    case .denied:
+      completion(false)
+    case .undetermined:
+      AVAudioSession.sharedInstance().requestRecordPermission({ granted in
+        completion(granted)
+      })
+    @unknown default:
+      completion(false)
+    }
+  }
+
+  static func getVoicePermissions(completion: @escaping (Bool) -> Void) {
     SFSpeechRecognizer.requestAuthorization { authStatus in
       OperationQueue.main.addOperation {
         switch authStatus {
@@ -148,36 +167,57 @@ public class Voice {
       }
     }
   }
-    
-    func train(locale: String, phrases: [String], graphemeToPhonems: [[String]]) async -> Void {
-        if #available(iOS 17, *) {
-            do {
-                let data = SFCustomLanguageModelData(locale: Locale(identifier: locale), identifier: "com.philip1986.dragonreading", version: "1.0")
-                
-                phrases.forEach { phrase in
-                    data.insert(phraseCount: SFCustomLanguageModelData.PhraseCount(phrase: phrase, count: 1000))
-                }
-                
-                graphemeToPhonems.forEach { mapping in
-                    data.insert(term: SFCustomLanguageModelData.CustomPronunciation(grapheme: mapping[0], phonemes: [mapping[1]]))
-                }
-                
-                let fileName = "custom.bin"
-                let docDirURL = try! FileManager.default.url(for: .cachesDirectory, in: .allDomainsMask, appropriateFor: nil, create: true)
-                let path = docDirURL.appendingPathComponent(fileName)
-                try await data.export(to: path)
-            
-                try await SFSpeechLanguageModel.prepareCustomLanguageModel(for: path, clientIdentifier: "com.philip1986.dragonreading", configuration: self.lmConfiguration)
-                return
-            } catch {
-                return
-            }
-        }
+
+  static func getPermissions(completion: @escaping (Bool) -> Void) {
+    getMicPermissions { micPermissions in
+      if !micPermissions {
+        completion(false)
+        return
+      }
+      getVoicePermissions { voicePermissions in
+        completion(voicePermissions)
+      }
     }
+  }
+
+  func train(locale: String, phrases: [String], graphemeToPhonems: [[String]]) async {
+    if #available(iOS 17, *) {
+      do {
+        let data = SFCustomLanguageModelData(
+          locale: Locale(identifier: locale), identifier: "com.philip1986.dragonreading",
+          version: "1.0")
+
+        phrases.forEach { phrase in
+          data.insert(
+            phraseCount: SFCustomLanguageModelData.PhraseCount(phrase: phrase, count: 1000))
+        }
+
+        graphemeToPhonems.forEach { mapping in
+          data.insert(
+            term: SFCustomLanguageModelData.CustomPronunciation(
+              grapheme: mapping[0], phonemes: [mapping[1]]))
+        }
+
+        let fileName = "custom.bin"
+        let docDirURL = try! FileManager.default.url(
+          for: .cachesDirectory, in: .allDomainsMask, appropriateFor: nil, create: true)
+        let path = docDirURL.appendingPathComponent(fileName)
+        try await data.export(to: path)
+
+        try await SFSpeechLanguageModel.prepareCustomLanguageModel(
+          for: path, clientIdentifier: "com.philip1986.dragonreading",
+          configuration: self.lmConfiguration)
+        return
+      } catch {
+        return
+      }
+    }
+  }
 
   func startRecording(
     target: [[String]], timeout: Int, onDeviceRecognition: Bool,
     mistakeConfig: [String: Int],
+    firstItemDurationOffset: Int = 0,
     completion: @escaping (Error?, Bool?, String?, [[String: Any]]?) -> Void
   ) throws {
     print("start recording ...")
@@ -186,11 +226,12 @@ public class Voice {
     var isComplete = false
     var start: CFTimeInterval?
     var res: [[String: Any]] = target.map {
-        ["word": $0.first!, "duration": 0, "isCorrect": false, "mistake": false]
+      ["word": $0.first!, "duration": 0, "isCorrect": false, "mistake": false]
     }
     var part = StringAccumulator()
     var mistakeCount = 0
     var mistakeTimeout: Timer?
+    var firstItemDurationOffsetApplied = false
 
     if Voice.hasPermissions == false {
       return completion(NSError(domain: "No permissions", code: 0, userInfo: nil), nil, nil, nil)
@@ -201,34 +242,35 @@ public class Voice {
         NSError(domain: "Speech recognion is not supported!", code: 0, userInfo: nil), nil, nil, nil
       )
     }
-    
+
     try audioSession.setCategory(.record, mode: .measurement, options: .mixWithOthers)
     try audioSession.setActive(true, options: .notifyOthersOnDeactivation)
 
     inputNode = audioEngine.inputNode
     inputNode?.removeTap(onBus: 0)
-    
+
     let recordingFormat = inputNode?.inputFormat(forBus: 0)
-    if(recordingFormat?.channelCount == 0) {
-        return completion(NSError(domain: "", code: 500, userInfo: ["msg": "Audio device busy!"]), nil, nil, nil)
+    if recordingFormat?.channelCount == 0 {
+      return completion(
+        NSError(domain: "", code: 500, userInfo: ["msg": "Audio device busy!"]), nil, nil, nil)
     }
-    
+
     try inputNode?.installTap(onBus: 0, bufferSize: 512, format: recordingFormat) {
-        (buffer: AVAudioPCMBuffer, _: AVAudioTime) in
-        DispatchQueue.global(qos: .background).async {
-            let volume = Utils.getVolumeLevel(buffer: buffer)
-            if start == nil && volume > 1 {
-                start = CACurrentMediaTime()
-                
-            }
-            self.sendEvent("onVolumeChange", ["volume": volume])
+      (buffer: AVAudioPCMBuffer, _: AVAudioTime) in
+      DispatchQueue.global(qos: .background).async {
+        let volume = Utils.getVolumeLevel(buffer: buffer)
+        if start == nil && volume > 1 {
+          start = CACurrentMediaTime()
+
         }
-        self.recognitionRequest?.append(buffer)
+        self.sendEvent("onVolumeChange", ["volume": volume])
+      }
+      self.recognitionRequest?.append(buffer)
     }
-    
+
     audioEngine.prepare()
     try audioEngine.start()
-      
+
     recognitionRequest = SFSpeechAudioBufferRecognitionRequest()
     guard let recognitionRequest = recognitionRequest else {
       fatalError("Unable to create a SFSpeechAudioBufferRecognitionRequest object")
@@ -239,10 +281,10 @@ public class Voice {
 
     recognitionRequest.requiresOnDeviceRecognition =
       onDeviceRecognition == true && speechRecognizer?.supportsOnDeviceRecognition == true
-      
-      if #available(iOS 17, *) {
-          recognitionRequest.customizedLanguageModel = self.lmConfiguration
-      }
+
+    if #available(iOS 17, *) {
+      recognitionRequest.customizedLanguageModel = self.lmConfiguration
+    }
 
     speechRecognizer?.queue.qualityOfService = .userInteractive
 
@@ -274,7 +316,9 @@ public class Voice {
 
           if !isComplete {
             isComplete = true
-            completion(NSError(domain: "Timeout", code: 0, userInfo: ["msg": "Timeout"]), false, transcription.formattedString, res)
+            completion(
+              NSError(domain: "Timeout", code: 0, userInfo: ["msg": "Timeout"]), false,
+              transcription.formattedString, res)
           }
         }
 
@@ -286,10 +330,13 @@ public class Voice {
           .enumerated().forEach { (i, el) in
             if (res[i]["isCorrect"] as? Bool) == false {
               let now = CACurrentMediaTime()
+              let duration = Int((now - (start ?? now - 99)) * 1000)
               res[i]["isCorrect"] = true
-              res[i]["duration"] = Int((now - (start ?? now - 99)) * 1000)
+              res[i]["duration"] =
+                firstItemDurationOffsetApplied ? duration : duration - firstItemDurationOffset
               start = now
               wordsAdded += 1
+              firstItemDurationOffsetApplied = true
             }
           }
 
@@ -313,12 +360,12 @@ public class Voice {
         } else {
           if mistakeCount >= mistakeConfig["mistakeLimit"]! {
             mistakeCount = 0
-              res.firstIndex(where: { $0["isCorrect"] as! Bool == false }).let { i in
-                res[i]["mistake"] = true
-                  self.sendEvent("onMistake", ["word": res[i]["word"] as! String, "reason": "tooManyMistakes"])
-              }
-              
-            
+            res.firstIndex(where: { $0["isCorrect"] as! Bool == false }).let { i in
+              res[i]["mistake"] = true
+              self.sendEvent(
+                "onMistake", ["word": res[i]["word"] as! String, "reason": "tooManyMistakes"])
+            }
+
           } else {
 
             if mistakeConfig["timeLimit"] ?? 0 > 0 && mistakeTimeout?.isValid != true {
@@ -339,32 +386,33 @@ public class Voice {
   }
 
   func isTarget(text: String, target: [[String]]) -> Bool {
-      return Utils.commonWords(in: target, containedIn: text) == target.map { $0.first?.lowercased() ?? "" }.joined(separator: " ")
+    return Utils.commonWords(in: target, containedIn: text)
+      == target.map { $0.first?.lowercased() ?? "" }.joined(separator: " ")
   }
 
   func stopRecording() {
     print("stop recording ...")
     recognitionTask?.cancel()
     timeout?.invalidate()
-      
+
     audioEngine.stop()
     inputNode?.removeTap(onBus: 0)
     Thread.sleep(forTimeInterval: 0.1)
-      var sessionSet = false
-      var c = 0
-      while (sessionSet == false) {
-          do {
-              try audioSession.setCategory(.playback, mode: .voicePrompt, options: .mixWithOthers)
-              sessionSet = true
-          } catch let e {
-              print(e)
-              Thread.sleep(forTimeInterval: 0.1)
-              c += 1
-              if(c > 10) {
-                  sessionSet = true
-              }
-          }
+    var sessionSet = false
+    var c = 0
+    while sessionSet == false {
+      do {
+        try audioSession.setCategory(.playback, mode: .voicePrompt, options: .mixWithOthers)
+        sessionSet = true
+      } catch let e {
+        print(e)
+        Thread.sleep(forTimeInterval: 0.1)
+        c += 1
+        if c > 10 {
+          sessionSet = true
+        }
       }
+    }
 
     recognitionRequest?.endAudio()
     recognitionRequest = nil
@@ -373,8 +421,8 @@ public class Voice {
 }
 
 class Utils {
-    public static func commonWords(in string1: [[String]], containedIn string2: String) -> String {
-    let words1 = string1.map { $0.map { $0.lowercased()} }
+  public static func commonWords(in string1: [[String]], containedIn string2: String) -> String {
+    let words1 = string1.map { $0.map { $0.lowercased() } }
     let words2 = string2.lowercased().split(separator: " ")
 
     var commonWords: [String] = []
@@ -383,8 +431,8 @@ class Utils {
 
     for word1 in words1 {
       while let word2 = iterator2.next() {
-          if word1.contains(where: { word2.contains($0)}) {
-              currentWords.append(word1.first ?? "")
+        if word1.contains(where: { word2.contains($0) }) {
+          currentWords.append(word1.first ?? "")
           break
         } else if !currentWords.isEmpty {
           commonWords.append(currentWords.joined(separator: " "))
