5e9546721c3f4311050da63c1ee5f278ebe50910
Philip <philip@lese-drache.de>
2024-10-14 19:13:23 +0200

    add Android

--- android/src/main/java/expo/modules/blitzlesenvoice/BlitzlesenVoiceView.kt
+++
@@ -1,7 +0,0 @@
-package expo.modules.blitzlesenvoice
-
-import android.content.Context
-import expo.modules.kotlin.AppContext
-import expo.modules.kotlin.views.ExpoView
-
-class BlitzlesenVoiceView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
